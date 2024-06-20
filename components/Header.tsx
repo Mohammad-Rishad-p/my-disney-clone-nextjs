66bea8802106c4482277071caef9de655411c35f
Mohammad-Rishad-p <r25richu@gmail.com>
2024-06-20 15:08:27 +0530

    before umdb

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { ThemeToggler } from './ThemeToggler'
+import SearchInput from './SearchInput'
 
 function Header() {
   return (
-    <header>
+    <header className='fixed w-full z-20 flex items-center
+     justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900'>
       {/* first section */}
-      <Link href='/'>
+      <Link href='/' className=' mr-10'>
         <Image
           src="https://links.papareact.com/a943ae"
           alt='Disney logo'
@@ -19,9 +21,10 @@ function Header() {
       </Link>
 
       {/* second section */}
-      <div>
+      <div className='flex space-x-2'>
         {/* Genre dropdown */}
         {/* search input */}
+        <SearchInput />
         {/* theme triiger */}
         <ThemeToggler />
       </div>
